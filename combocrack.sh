f0f329d883d3b66bf19bce7c45fdb9e7071e0fdc
MasterChenb0x <artofmorellc@gmail.com>
2015-04-25 21:49:14 +0000

    This is an intial commit to the Combo Crack project as a test to make sure I have git figured out

---
+++ combocrack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##########################################
+# MasterChen's Master Combo Lock Crakcer #
+# Version 1.0a 2015/04/25                #
+##########################################
+
+# This is a script to aid in cracking Master Combination Locks
+# It brings the possibilities of 64,000 down to 80 possible combinations
+
+echo "Enter the last number of the combination"
+
