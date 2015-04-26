e1ad0211d098564cf61569e7de9189934e76929a
MasterChenb0x <artofmorellc@gmail.com>
2015-04-26 17:07:46 +0000

    Almost done eith first phase.
    Need to mess with final output, and automatically eliminate two more second number possibilities.

--- combocrack.sh
+++ combocrack.sh
@@ -9,4 +9,43 @@
 # It brings the possibilities of 64,000 down to 80 possible combinations
 
 echo "Enter the last number of the combination"
+read LASTNUM
+echo "The Last Number of the Combination is: $LASTNUM"
+
+#Find the first number possibilities and put them into an array
+echo "First Number possibilities:"
+counter=0
+i=$(( $LASTNUM % 4 ))
+while [ $i -lt 40 ]; do
+	first_num_poss[$counter]=$i
+	echo ${first_num_poss[$counter]}
+	let counter+=1
+	let i+=4
+done
+
+#Find the second number possibilities and put them into an array
+echo "Second Number possibilities:"
+counter=0
+i=$(($(( $LASTNUM % 4 ))-2))
+
+#This if statement gets rid of the small chance of a negative number
+if [ $i -lt 0 ] 
+then
+	let i+=4
+fi
+
+while [ $i -lt 40 ]; do
+        second_num_poss[$counter]=$i
+        echo ${second_num_poss[$counter]}
+        let counter+=1
+        let i+=4
+done
+
+#Put the arrays together and what do we get? Possible Combinations!
+for a in ${first_num_poss[@]} ; do
+	for b in ${second_num_poss[@]} ; do
+		echo "${first_num_poss[$a]}-${second_num_poss[$b]}-$LASTNUM"
+	done
+done
+
 
